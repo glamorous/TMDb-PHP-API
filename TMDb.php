76c583ba384a8d064d47dbf0dcd7a07d7801a99c
Jonas De Smet <jonas@glamorous.be>
2009-11-09 23:41:32 +0100

    Initial commit

---
+++ TMDb.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * TMDb PHP API class - API 'themoviedb.org'
+ * API Documentation: http://api.themoviedb.org/2.1/
+ * 
+ * @author Jonas De Smet - Glamorous
+ * @since 09.11.2009
+ * @copyright Jonas De Smet - Glamorous
+ * @license BSD http://www.opensource.org/licenses/bsd-license.php
+ */
+
+class TMDb
+{
+	
+}
+?>
